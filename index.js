c77129e028da5761ad8fb41f7eb987cf212582b0
Bradley Scott <bradley.s.scott@gmail.com>
2016-08-23 19:34:11 +1000

    Decoupled from scoreProcessor

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var debug = require('debug')('next-ball-processor');
 var Promise = require('bluebird');
 var eventStore = Promise.promisifyAll(require('./eventstore.js'));
 var entities = Promise.promisifyAll(require('./entities.js'));
-var scoreProcessor = Promise.promisifyAll(require('./scoreProcessor.js'));
 var eventProcessors = require('./eventProcessors.js');
 var _ = require('underscore');
 
@@ -18,26 +17,37 @@ app.get('/', function(req, res) {
         return res.status(400).send(error);
     }
 
-    var lastBall, limitedOvers, wickets;
+    var matchEvents = [];
+    var lastBall = {};
+    
     eventStore.getMatchEventsAsync(match)
         .then(function(events) {
             if(events.length == 0) {
-                debug('No events for this match');
-                return res.status(404).send();
+                var message = 'No events for this match';
+                debug(message);
+                return res.status(404).send(message);
             }
-
+            
+            matchEvents = events;
             lastBall = events[events.length - 1];
             return entities.getMatchDetailsAsync(match);
         })
         .then(function(matchDetails) {
-            limitedOvers = matchDetails.limitedOvers;
+            var limitedOvers = matchDetails.limitedOvers;
             if(matchDetails.limitedOvers) debug('This match is limited to %s overs', matchDetails.limitedOvers);
-
-            return scoreProcessor.getScoreAsync(match);
-        })
-        .then(function(score){
             var innings = lastBall.ball.innings;
-            wickets = score.innings[innings].wickets;
+
+            // Work out number of wickets down
+            var inningsEvents = _(matchEvents).filter(function(e){
+                return e.ball.innings == innings;
+            });
+            var wickets = _(matchEvents).filter(function(e){
+                if(e.eventType != 'delivery' && e.eventType != 'noBall' && e.eventType != 'wide' || 
+                    e.eventType != 'bye' && e.eventType == 'legBye') return true;
+                else return false;
+            });
+            debug('%s wickets in this innings', wickets.length);
+            wickets = wickets.length;
 
             // Default next ball information
             var response = {
